1aab4258ebe9c6580d8c0a42f078815787a1b74d
Vipul Puttaswamy Gowda <vipulp@pdx.edu>
2024-08-05 21:50:05 -0700

    Updated experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,7 +4,7 @@ export default function Experience() {
   return (
     <div className="rows-2 sm:rows-3 gap-4 my-8">
       <h2 className="font-bold text-2xl mb-8 tracking-tighter">
-        Previously I've Worked at
+        Work Experience
       </h2>
       <Timeline/>
     </div>
